1c67de38664dbe51ed9595a3c5de59a10f1b9e31
unknown <77588948+deivmaik@users.noreply.github.com>
2021-03-10 03:07:47 -0400

    16-Weather App 'CSS and Initial Markup done'

--- Backup/16-WeatherApp/weather-app/src/App.js
+++ Backup/16-WeatherApp/weather-app/src/App.js
@@ -1,9 +1,45 @@
+import React from 'react';
 
+const api = {
+  key: "bdb3e02eff4a2b64da0f22fb3072cc64",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
+
+  const dateBuilder = d => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()]
+    let date = d.getDate()
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+
+
   return (
-    <div className="App">
-      <h1>Weather App</h1>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input 
+          type="text" 
+          className="search-bar"
+          placeholder="Type a place to start."
+          />
+        </div>
+        <div className="location-box">
+          <div className="location">Buenos Aires, AR </div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">25C</div> 
+          <div className="weather">Windy</div>
+        </div>
+      </main>
     </div>
   );
 }
