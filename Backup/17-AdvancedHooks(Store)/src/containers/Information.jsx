73b21d064de98933880000013c82ff9d743e98d1
unknown <77588948+deivmaik@users.noreply.github.com>
2021-03-11 06:04:37 -0400

    17-Custom hook useRef

--- Backup/17-AdvancedHooks(Store)/src/containers/Information.jsx
+++ Backup/17-AdvancedHooks(Store)/src/containers/Information.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useRef, useContext } from 'react';
 import { Link } from 'react-router-dom'
+import AppContext from '../context/AppContext'
 import '../styles/components/Information.css'
 
-const Information = () => {
+const Information = ({ history }) => {
+  const { state, addToBuyer } = useContext(AppContext)
+  const form = useRef(null)
+  const { cart } = state
+
+  const handleSubmit = () => {
+    const formData = new FormData(form.current)
+    const buyer = {
+      'name' : formData.get('name'),
+      'email' : formData.get('email'),
+      'address': formData.get('address'),
+      'apt' : formData.get('apt'),
+      'city' : formData.get('city'),
+      'country' :formData.get('country') ,
+      'state' :formData.get('state') ,
+      'cp' : formData.get('cp'),
+      'phone' : formData.get('phone'),
+
+    }
+    addToBuyer(buyer)
+    history.push('/checkout/payment')
+  }
+
   return (
     <div className="Information">
       <div className="Information-content">
@@ -10,11 +33,11 @@ const Information = () => {
           <h2>Contact Information:</h2>
         </div>
         <div className="Information-form">
-          <form action="">
+          <form ref={form}>
             <input type="text" placeholder="Full Name" name="name"/>
             <input type="text" placeholder="Email" name="email"/>
             <input type="text" placeholder="Address" name="address"/>
-            <input type="text" placeholder="c/o" name="apto"/>
+            <input type="text" placeholder="apt" name="apt"/>
             <input type="text" placeholder="City Name" name="city"/>
             <input type="text" placeholder="Country" name="country"/>
             <input type="text" placeholder="State" name="state"/>
@@ -24,23 +47,26 @@ const Information = () => {
         </div>
         <div className="Information-buttons">
           <div className="Information-back">
+            <Link to="/checkout">
             Go back
+            </Link>
           </div>
-          <Link to="/checkout/payment">
           <div className="Information-next">
-            Pay
+            <button type="button" onClick={handleSubmit}>Pay</button>
           </div>
-          </Link>
         </div>
       </div>
       <div className="Information-sidebar">
         <h3>Orders:</h3>
-        <div className="Information-item">
+        {cart.map((item)=>(
+          <div className="Information-item" key={item.title}>
           <div className="Information-element">
-            <h4>Item Name</h4>
-            <span>10$</span>
+            <h4>{item.title}</h4>
+            <span>{item.price}$</span>
           </div>
         </div>
+        ))}
+        
       </div>
     </div>
   );
