acec9865d907efe8d78e4bc95ebe4bfc4c7e0e69
unknown <77588948+deivmaik@users.noreply.github.com>
2021-03-11 01:42:54 -0400

    17-Checkout pageimplemented

--- Backup/17-AdvancedHooks(Store)/src/components/Footer.jsx
+++ Backup/17-AdvancedHooks(Store)/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import '../styles/components/Footer.css'
 const Footer = () => {
   return (
     <div className="Footer">
-      <p className="Footer-title">Platzi Conf Merch</p>
+      <p className="Footer-title">Michael</p>
       <p className="Footer-copy">Todos los Izquierdos Reservados</p>
     </div>
   );
--- Backup/17-AdvancedHooks(Store)/src/containers/Checkout.jsx
+++ Backup/17-AdvancedHooks(Store)/src/containers/Checkout.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import '../styles/components/Checkout.css'
 
 const Checkout = () => {
   return (
-    <h1>Checkout</h1>
+    <div className="Checkout">
+      <div className="Checkout-content">
+        <h3>Checkout</h3>
+        <div className="Checkout-item">
+          <div className="Checkout-element">
+            <h4>Item Name</h4>
+            <span>$10</span>
+          </div>
+          <button type="button">Delete</button>
+        </div>
+      </div>
+      <div className="Checkout-sidebar">
+        <h3>Total Price: $10</h3>
+        <button type="button">Continue</button>
+      </div>
+    </div>
   );
 }
 
